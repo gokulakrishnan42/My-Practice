cccb5553a515a218a88d3caa5ecbb2fa58a29dd5
Gokula Krishnan <gokulwhoami@gmail.com>
2025-10-22 20:48:22 +0530

    This is MVC architecture
    this is my learning page,how MVC works!

---
+++ ShoppingApp/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShoppingApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+    }
+}
