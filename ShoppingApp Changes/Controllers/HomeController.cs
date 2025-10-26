3a21321023514a498a610ae1b598b97ba8343107
Gokula Krishnan <gokulwhoami@gmail.com>
2025-10-26 12:47:11 +0530

    this is some changes for MVC Architecture of ShoppingApp in View(ViewBag,ViewData,TempView)

---
+++ ShoppingApp Changes/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShoppingApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<ShoppingApp.Models.User> ousers = new List<ShoppingApp.Models.User>()
+        {
+
+        new ShoppingApp.Models.User() {Name = "Gojo",ImagePath = "Images/Gojo.jpg",Desc = "Welcome to my Page" },
+        new ShoppingApp.Models.User() {Name = "Employee",ImagePath = "Images/images.jpg",Desc = "Welcome to my Page" }
+
+
+        };
+            ViewData["Name"] = "Gokul";
+            ViewData["listofusers"] = ousers;
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ShoppingApp Changes/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace ShoppingApp.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string ImagePath { get; set; }
+        public string Desc { get; set; }
+    }
+}
